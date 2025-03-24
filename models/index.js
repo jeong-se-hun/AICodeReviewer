eaaf7c480edcf6062a1abc8c2fd3010a41fb43aa
jeong-se-hun <tpgns933@gmail.com>
2025-03-24 17:21:11 +0900

    Update index.js
    오타 수정

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 export { getGeminiReview } from "./gemini.js";
 export { getMistralReview } from "./mistral.js";
 export { getAzureReview } from "./azure.js";
-export { getOpenAiReview } from "./openai.js";
+export { getOpenAiReview } from "./openAi.js";
