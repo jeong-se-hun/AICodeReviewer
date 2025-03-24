8e5262c895bd3cdaa47ec66c3a828604f6a6a61b
jeong-se-hun <tpgns933@gmail.com>
2025-03-24 17:05:01 +0900

    openAi 연결 추가

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 export { getGeminiReview } from "./gemini.js";
 export { getMistralReview } from "./mistral.js";
 export { getAzureReview } from "./azure.js";
+export { getOpenAiReview } from "./openai.js";
