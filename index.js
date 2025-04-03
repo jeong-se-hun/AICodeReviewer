588c9afa73fc2ee4b2006ffdda2b3828b79bdd36
jeong-se-hun <tpgns933@gmail.com>
2025-04-03 12:37:05 +0900

    테스트 목적 콘솔 출력 추가

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { getCommitDetails, getPRDiff, postComment } from "./utils/githubApi.js";
-import { AI_MODEL_PROVIDER } from "./utils/env.js";
+import { AI_MODEL_PROVIDER, GITHUB_EVENT_ACTION } from "./utils/env.js";
 
 import {
   getGeminiReview,
@@ -21,19 +21,20 @@ const reviewFunctionsMap = {
 const reviewFunction = reviewFunctionsMap[AI_MODEL_PROVIDER] || null;
 
 async function runReview() {
+  console.log("GITHUB_EVENT_ACTION", GITHUB_EVENT_ACTION);
   try {
-    // PR diff, 커밋 정보 가져오기
-    const [diff, commitDetails] = await Promise.all([
-      getPRDiff(),
-      getCommitDetails(),
-    ]);
-
     // 리뷰 함수 유효성 체크
     if (!reviewFunction) {
       throw new Error(
         `${AI_MODEL_PROVIDER}는 지원하지 않는 AI_MODEL_PROVIDER입니다.`
       );
     }
+
+    // PR diff, 커밋 정보 가져오기
+    const [diff, commitDetails] = await Promise.all([
+      getPRDiff(),
+      getCommitDetails(),
+    ]);
 
     // 코드 리뷰 생성
     const reviewComment = await reviewFunction({ diff, commitDetails });
