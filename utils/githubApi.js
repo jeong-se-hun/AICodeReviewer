b761861a9eda2bb1f7be6e349959e4edad2c9f54
jeong-se-hun <tpgns933@gmail.com>
2025-04-03 14:23:39 +0900

    url 변경 하여 값 테스트

--- utils/githubApi.js
+++ utils/githubApi.js
@@ -41,7 +41,8 @@ export async function getPRDiff() {
   ) {
     // PR이 업데이트 됐을 때의 diff 가져오기
     console.log("PR이 업데이트 됐을 때의 diff 가져오기"); // TODO 테스트 후 삭제 예정
-    const diffUrl = `https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${COMMIT_AFTER}`;
+    // const diffUrl = `https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${COMMIT_AFTER}`;
+    const diffUrl = `https://api.github.com/repos/${GITHUB_REPOSITORY}/commits`;
     response = await fetchGitHubApi(diffUrl, {
       accept: "application/vnd.github.v4.diff",
     });
