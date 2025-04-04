166430101eed7175b71a3573b3f8b889f52ddc84
jeong-se-hun <tpgns933@gmail.com>
2025-04-04 21:32:21 +0900

    Update mistral.js
    - 테스트 확인용 출력 추가

--- models/mistral.js
+++ models/mistral.js
@@ -3,10 +3,11 @@ import { AI_API_KEY, AI_MODEL } from "../config/env.js";
 import { getPrompt } from "../utils/prompt.js";
 import { TIMEOUT } from "../config/constants.js";
 
-const client = new Mistral({ apiKey: AI_API_KEY, timeout: TIMEOUT });
+const client = new Mistral({ apiKey: AI_API_KEY, timeout: 10 });
 
 // Mistral API 호출
 export async function getMistralReview(reviewData) {
+  console.log("Mistral API 호출"); // TODO: remove this line
   const prompt = getPrompt(reviewData);
 
   const chatResponse = await client.chat.complete({
