36a7aedb90ea44ca5ffe40838fc49ece7e53070f
jeong-se-hun <tpgns933@gmail.com>
2025-04-04 21:41:04 +0900

    Update mistral.js
    - timeout 테스트 재 시도
    - 에러 처리 추가

--- models/mistral.js
+++ models/mistral.js
@@ -1,19 +1,37 @@
 import { Mistral } from "@mistralai/mistralai";
 import { AI_API_KEY, AI_MODEL } from "../config/env.js";
 import { getPrompt } from "../utils/prompt.js";
-import { TIMEOUT } from "../config/constants.js";
+import { ERROR_MESSAGES, TIMEOUT } from "../config/constants.js";
 
-const client = new Mistral({ apiKey: AI_API_KEY, timeout: 10 });
+const client = new Mistral({ apiKey: AI_API_KEY });
 
 // Mistral API 호출
 export async function getMistralReview(reviewData) {
-  console.log("Mistral API 호출"); // TODO: remove this line
-  const prompt = getPrompt(reviewData);
+  try {
+    console.log("mistral api 호출"); // TODO 삭제 예정
+    const prompt = getPrompt(reviewData);
 
-  const chatResponse = await client.chat.complete({
-    model: AI_MODEL,
-    messages: [{ role: "user", content: prompt }],
-  });
+    const chatResponse = await client.chat.complete(
+      {
+        model: AI_MODEL,
+        messages: [{ role: "user", content: prompt }],
+      },
+      {
+        timeout: TIMEOUT,
+      }
+    );
 
-  return chatResponse.choices[0].message.content;
+    if (
+      !chatResponse ||
+      !chatResponse.choices ||
+      chatResponse.choices.length === 0 ||
+      !chatResponse.choices[0].message?.content
+    ) {
+      throw new Error(ERROR_MESSAGES.AI_EMPTY_RESPONSE);
+    }
+
+    return chatResponse.choices[0].message.content;
+  } catch (error) {
+    throw new Error(`${ERROR_MESSAGES.AI_REVIEW_FAILED}: ${error.message}`);
+  }
 }
